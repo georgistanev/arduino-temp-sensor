ba04a2d3b4cceb501aa8de4ca104c812a9eb7fce
Georgi Stanev <georgi.stanev@gmail.com>
2018-01-06 10:52:29 -0500

    Read temp through DHT library

--- tempSensor.ino
+++ tempSensor.ino
@@ -1,16 +1,28 @@
-#include <ESP8266WiFi.h>tials
+#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <DHT.h>
+
+DHT dht(D2, DHT22);
+
 #include "serial.h"
 #include "wifi.h"
 #include "http.h"
 #include "credentials.h"
 
 void setup() {
-	initSerial();
-	initWifi(WIFI_SSID, WIFI_PASS);
+  	initSerial();
+	  initWifi(WIFI_SSID, WIFI_PASS);
+
+    dht.begin();
 }
 
 void loop() {
-  httpRequest("http://google.com");
-  delay(10000);
+    float humidity = dht.readHumidity();
+    float temp = dht.readTemperature();
+    
+    Serial.println("Read data:");
+    Serial.println(temp);
+    Serial.println(humidity);
+  
+    delay(1000);
 }
